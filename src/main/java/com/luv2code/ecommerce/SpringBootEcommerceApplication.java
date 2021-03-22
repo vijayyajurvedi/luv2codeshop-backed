6012d3c43427618835abd252dbcccfaff09ade92
Vijay Yajurvedi <vijay.yajurvedi@gmail.com>
2021-03-22 16:07:56 +0530

    initial commit

--- src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
+++ src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -3,9 +3,9 @@ package com.luv2code.ecommerce;
  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
  
-@EnableSwagger2
+ 
+ 
 @SpringBootApplication
 public class SpringBootEcommerceApplication {
 
--- src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -39,16 +39,16 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                                               HttpMethod.DELETE, HttpMethod.PATCH};
 
         // disable HTTP methods for ProductCategory: PUT, POST and DELETE
-        disableHttpMethods(Product.class, config, theUnsupportedActions);
-        disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
-        disableHttpMethods(Country.class, config, theUnsupportedActions);
-        disableHttpMethods(State.class, config, theUnsupportedActions);
+        //disableHttpMethods(Product.class, config, theUnsupportedActions);
+        //disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+       //disableHttpMethods(Country.class, config, theUnsupportedActions);
+        //disableHttpMethods(State.class, config, theUnsupportedActions);
 
         // call an internal helper method
         exposeIds(config);
 
         // configure cors mapping
-        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
+        //cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
     }
 
     private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
