355c53eb9a46147a4dea40951493ef83ce49b8ee
Vijay Yajurvedi <vijay.yajurvedi@gmail.com>
2021-03-27 14:21:48 +0530

    initial commit

--- src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
+++ src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -1,8 +1,6 @@
 package com.luv2code.ecommerce;
 
  
-import javax.annotation.Resource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
  
